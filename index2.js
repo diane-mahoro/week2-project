f7fab22706e50502bb54ddaa879884a94f1861e0
diane-mahoro <www.diammah7@gmail.com>
2019-07-18 18:53:06 +0200

    change in files

---
+++ index2.js
@@ -0,0 +1,79 @@
+/*function A(){
+    var date = document.getElementById("A1").value;
+    alert(date);
+
+};
+Day of the week (d) = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) mod 7
+document.getElementById("submit").onclick = function(){
+    alert(A());
+};
+var d=parseInt(prompt("enter the day"));
+var m=parseInt(prompt("enter the month"));
+var y=parseInt(prompt("enter the year"));
+alert("the century is " +c);
+*/
+function dayofweek(){
+    var input =document.getElementById('A1').value;
+    var x= new Date( input );
+    y=x.getFullYear();
+    m=x.getMonth();
+    m=m+1;
+    d=x.getDay();
+   /* function century(){
+        return parseInt(y/100) +1;
+    }*/
+    var c= parseInt(y/100) +1;
+    var gender= document.getElementById('A4').value;
+    var day=(((c/4) -2*c-1) + ((5*y/4)) + ((26*(m+1)/10)) + d) % 7;
+var dayOfWeek= parseInt(day);
+/*
+var dayOfWeek = parseInt(dayofweek());
+var gender=prompt("enter your gender");
+alert(gender);
+alert("day of week = " +dayOfWeek);
+if(gander=='' || d=='' || m=='' || y=''){
+alert("please insert something");
+}
+else */if (dayOfWeek == 0 && gender =='male'){
+ document.getElementById('A2').innerHTML ='Kwasi'
+}
+else if(dayOfWeek == 1 && gender=='male'){
+    document.getElementById('A2').innerHTML = 'Kwado';
+}
+else if(dayOfWeek == 2 && gender=='male'){
+    document.getElementById('A2').innerHTML ='Kwabena'
+}
+else if(dayOfWeek == 3 && gender=='male'){
+    document.getElementById('A2').innerHTML ='Kwafi'
+}
+else if(dayOfWeek == 4 && gender=='male'){
+    document.getElementById('A2').innerHTML ='Yaw';
+}
+else if(dayOfWeek == 5 && gender=='male'){
+    document.getElementById('A2').innerHTML ='Kofi';
+}
+else if(dayOfWeek == 6 && gender=='male'){
+    document.getElementById('A2').innerHTML ='Kwame';
+}
+else if(dayOfWeek == 0 && gender=='female'){
+    document.getElementById('A2').innerHTML ='Kosua';
+}
+else if(dayOfWeek == 1 && gender=='female'){
+    document.getElementById('A2').innerHTML ='Adwona';
+}
+else if(dayOfWeek == 2 && gender=='female'){
+    document.getElementById('A2').innerHTML ='Abenaa';
+}
+else if(dayOfWeek == 3 && gender=='female'){
+    document.getElementById('A2').innerHTML ='Akua';
+}
+else if(dayOfWeek == 4 && gender=='female'){
+    document.getElementById('A2').innerHTML ='Yaa';
+}
+else if(dayOfWeek == 5 && gender=='female'){
+    document.getElementById('A2').innerHTML ='Afua';
+}
+else{
+    document.getElementById('A2').innerHTML = 'Ama';
+}
+}
